bec93dd67697636e82e3e597b5725d214655ba5e
puchochiri <doawishfor@naver.com>
2023-03-14 21:56:33 +0900

    commit

---
+++ build/precache-manifest.b790a9b9ac615ae6c4724e64b1cc54de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8a11f34bfabd22b7d76",
+    "url": "/static/css/main.24379560.chunk.css"
+  },
+  {
+    "revision": "f8a11f34bfabd22b7d76",
+    "url": "/static/js/main.f8a11f34.chunk.js"
+  },
+  {
+    "revision": "5b0c1fd61327d865c6b6",
+    "url": "/static/js/1.5b0c1fd6.chunk.js"
+  },
+  {
+    "revision": "1d939816da8858e21a00",
+    "url": "/static/js/2.1d939816.chunk.js"
+  },
+  {
+    "revision": "b784ede9eaf89b0e2666",
+    "url": "/static/js/runtime~main.b784ede9.js"
+  },
+  {
+    "revision": "da56cfde9ca8cbe8fca8cb694dfa999d",
+    "url": "/index.html"
+  }
+];
--- src/app-config.js
+++ src/app-config.js
@@ -2,8 +2,10 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === "localhost") {
+if (hostname ==="localhost") {
   backendHost = "http://localhost:8082";
+} else { 
+  backendHost = "http://prod-todo-api-service-test.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
