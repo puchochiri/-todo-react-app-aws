a186a4b94f7e8f64b4410e5175fd1cdd9b926bcc
puchochiri <doawishfor@naver.com>
2023-04-01 20:28:18 +0900

    deploy

--- src/app-config.js
+++ src/app-config.js
@@ -5,8 +5,8 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname ==="localhost") {
   backendHost = "http://localhost:8080";
 } else { 
-  backendHost = "http://prod-todo-api-service-test.ap-northeast-2.elasticbeanstalk.com";
-  //backendHost = "https://api.oursecondlifes.com";
+  //backendHost = "http://prod-todo-api-service-test.ap-northeast-2.elasticbeanstalk.com";
+  backendHost = "https://api.oursecondlifes.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
